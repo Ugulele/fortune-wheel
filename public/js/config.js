929c593827dd9677d172345d0a49c65795b98314
Ugulele <adamczyk.blazej.96@gmail.com>
2024-01-25 00:23:47 +0100

    [fix] Replacing Shake with Slushy

--- public/js/config.js
+++ public/js/config.js
@@ -3,61 +3,61 @@ const largeFineAmount = 1000
 const smallFineAmount = 800
 
 export const rewardList = [
-  {
-    "text": "Brawo, oto twój burgerek!",
-    "result": "win"
-  },
-  {
-    "text": "Gratulacje, za następny zestaw płacisz",
-    "result": "fine",
-    "amount": largeFineAmount,
-    "currency": currency
-  },
-  {
-    "text": "Niestety, brzuszek dalej głodny",
-    "result": "lose"
-  },
-  {
-    "text": "Głodny nie jesteś sobą, graj dalej",
-    "result": "lose"
-  }, 
-  {
-    "text": "Nie poddawaj się!",
-    "result": "lose"
-  },
-  {
-    "text": "Gratulacje, smacznych skrzydełek",
-    "result": "win"
-  },
-  {
-    "text": "Szczęście jest co raz bliżej",
-    "result": "lose"
-  },
-  {
-    "text": "Wygrana dalej czeka na ciebie",
-    "result": "lose"
-  }, 
-  {
-    "text": "Gratulacje, za następny zestaw płacisz",
-    "result": "fine",
-    "amount": smallFineAmount,
-    "currency": currency
-  }, 
-  {
-    "text": "Shake nalany, zaraz będzie podany",
-    "result": "win"
-  },
-  {
-    "text": "Co zawiodło?",
-    "result": "lose"
-  },
-  {
-    "text": "Spragniony, o to twoja E-Cola!",
-    "result": "win"
-  },
-  {
-    "text": "Zakręciłeś za mocno i koło się zepsuło",
-    "result": "broken"
-  },
+	{
+		"text": "Brawo, oto twój burgerek!",
+		"result": "win",
+	},
+	{
+		"text": "Gratulacje, za następny zestaw płacisz",
+		"result": "fine",
+		"amount": largeFineAmount,
+		"currency": currency,
+	},
+	{
+		"text": "Niestety, brzuszek dalej głodny",
+		"result": "lose",
+	},
+	{
+		"text": "Głodny nie jesteś sobą, graj dalej",
+		"result": "lose",
+	},
+	{
+		"text": "Nie poddawaj się!",
+		"result": "lose",
+	},
+	{
+		"text": "Gratulacje, smacznych skrzydełek",
+		"result": "win",
+	},
+	{
+		"text": "Szczęście jest co raz bliżej",
+		"result": "lose",
+	},
+	{
+		"text": "Wygrana dalej czeka na ciebie",
+		"result": "lose",
+	},
+	{
+		"text": "Gratulacje, za następny zestaw płacisz",
+		"result": "fine",
+		"amount": smallFineAmount,
+		"currency": currency,
+	},
+	{
+		"text": "Slushy nalane, zaraz będzie podane",
+		"result": "win",
+	},
+	{
+		"text": "Co zawiodło?",
+		"result": "lose",
+	},
+	{
+		"text": "Spragniony, o to twoja E-Cola!",
+		"result": "win",
+	},
+	{
+		"text": "Zakręciłeś za mocno i koło się zepsuło",
+		"result": "broken",
+	},
 ]
 
